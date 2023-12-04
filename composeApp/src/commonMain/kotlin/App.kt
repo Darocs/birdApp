b6c896efd7b770631b6b441595b860b418c2fa68
Darocs <Darocs@mail.ru>
2023-12-04 15:08:04 +0300

    Intermediate result

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,25 +1,27 @@
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import dev.icerock.moko.mvvm.compose.getViewModel
+import dev.icerock.moko.mvvm.compose.viewModelFactory
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
-        var greetingText by remember { mutableStateOf("Hello World!") }
+        /*var greetingText by remember { mutableStateOf("Hello World!") }
         var showImage by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             Button(onClick = {
@@ -34,6 +36,49 @@ fun App() {
                     null
                 )
             }
+        }*/
+        val birdsViewModel = getViewModel(Unit, viewModelFactory { BirdViewModel() })
+        val uiState by birdsViewModel.uiState.collectAsState()
+
+        LaunchedEffect(birdsViewModel) {
+            birdsViewModel.updateImages()
+        }
+
+        BirdsPage(uiState)
+    }
+}
+
+@Composable
+fun BirdsPage(uiState: BirdsUiState) {
+    Column {
+        Row (
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            modifier = Modifier.fillMaxWidth().padding(5.dp)
+        ){
+
+        }
+
+        AnimatedVisibility(visible = uiState.images.isNotEmpty()) {
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(180.dp),
+                horizontalArrangement = Arrangement.spacedBy(5.dp),
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+                modifier = Modifier.fillMaxSize().padding(horizontal = 5.dp)
+            ) {
+                items(uiState.images) { image ->
+                    BirdImageCell(image)
+                }
+            }
         }
     }
+}
+
+@Composable
+fun BirdImageCell(image: BirdImage) {
+    KamelImage(
+        resource = asyncPainterResource(data = "https://sebi.io/demo-image-api/${image.path}"),
+        contentDescription = "${image.category} by ${image.author}",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier.fillMaxWidth().aspectRatio(1.0f)
+    )
 }
